772a0c805bf28bf60ee599abdfdcbaf2b4877407
Thais <tlnunes@thoughtworks.com>
2020-08-03 00:04:55 -0300

    changes logo

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../../assets/img/thaflix-logo.png'
+import Logo from '../../assets/img/educaflix-logo.png'
 import './Menu.css';
 import Button from '../Button';
 
