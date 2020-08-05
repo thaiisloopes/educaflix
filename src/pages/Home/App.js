88b51630d950be8b39e3d72dfd7ac62e89a7568c
Thais <tlnunes@thoughtworks.com>
2020-08-05 10:06:27 -0300

    starts using data from backend

--- src/pages/Home/App.js
+++ src/pages/Home/App.js
@@ -1,38 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Menu from '../../components/Menu';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
+import PageDefault from '../../components/PageDefault'
 import Footer from '../../components/Footer';
 import dadosIniciais from '../../data/dados_iniciais.json';
 import categoriasRepository from '../../repositories/categorias'
 
 function Home() {
+    const [dadosIniciais, setDadosIniciais] = useState([]);
+
     useEffect(() => {
         categoriasRepository.getAllWithVideos()
             .then((categoriasComVideos) => {
-                console.log(categoriasComVideos);
+                setDadosIniciais(categoriasComVideos);
             })
             .catch((err) => {
                 console.log(err.message);
             });
-    });
+    }, []);
 
   return (
-    <div style={{background: "#141414"}}>
-    	<Menu />
+    <PageDefault>
+        {dadosIniciais.length === 0 && (
+            <div>
+                Loading...
+            </div>
+        )}
 
-    	<BannerMain
-    		videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-    		url={dadosIniciais.categorias[0].videos[0].url}
-    		videoDescription={ "O que é Front-end? Trabalhando na área"}
-    	/>
+        {dadosIniciais.length >= 1 && (
+            <>
+            	<BannerMain
+            		videoTitle={dadosIniciais[0].videos[0].titulo}
+            		url={dadosIniciais[0].videos[0].url}
+            		videoDescription={ "O que é Front-end? Trabalhando na área"}
+            	/>
 
-    	<Carousel
-    		ignoreFirstVideo
-    		category={dadosIniciais.categorias[0]}
-    	/>
+            	<Carousel
+            		ignoreFirstVideo
+            		category={dadosIniciais[0]}
+            	/>
+            </>
+        )};
 
-    	<Carousel
+    	{/* <Carousel
     		category={dadosIniciais.categorias[1]}
     	/>
 
@@ -46,14 +57,9 @@ function Home() {
 
         <Carousel
             category={dadosIniciais.categorias[4]}
-        />
+        /> */} 
 
-        <Carousel
-            category={dadosIniciais.categorias[5]}
-        />
-
-        <Footer />
-    </div>
+    </PageDefault>
   );
 }
 
