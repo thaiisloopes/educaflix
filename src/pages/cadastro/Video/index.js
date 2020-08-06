6c3449e1a681ccf462d72ab547787865974d498c
Thais <tlnunes@thoughtworks.com>
2020-08-05 21:53:15 -0300

    does first video register version

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -4,11 +4,14 @@ import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
 import useForm from '../../../hooks/useForm';
 import Button from '../../../components/Button';
+import videosRepository from '../../../repositories/videos';
 
 function CadastroVideo() {
   const history = useHistory();
   const { funcaoHandler, valores } = useForm({
-
+    titulo: 'Vídeo padrão',
+    url: 'https://www.youtube.com/watch?v=f8a-qwKC5yk',
+    categoria: 'Front End',
   });
 
   return (
@@ -17,14 +20,35 @@ function CadastroVideo() {
 
       <form onSubmit={(event) => {
         event.preventDefault();
-        alert('Vídeo cadastrado com sucesso!!');
-        history.push('/');
+
+        videosRepository.create({
+          titulo: valores.titulo,
+          url: valores.url,
+          categoria: 1,
+        })
+          .then(() => {
+            history.push('/');
+          });
       }}
       >
         <FormField
           label="Título do Vídeo"
           name="titulo"
           value={valores.titulo}
+          onChange={funcaoHandler}
+        />
+
+        <FormField
+          label="URL"
+          name="url"
+          value={valores.url}
+          onChange={funcaoHandler}
+        />
+
+        <FormField
+          label="Categoria"
+          name="categoria"
+          value={valores.categoria}
           onChange={funcaoHandler}
         />
 
