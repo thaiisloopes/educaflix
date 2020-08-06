8afaf60c237a496224e19d21b97e148a11b95896
Thais <tlnunes@thoughtworks.com>
2020-08-06 17:46:19 -0300

    removes blank space from prod url

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export const URL_BACKEND = window.location.hostname.includes('localhost')
 	? 'http://localhost:8080'
-	: 'https://educaflix-app.herokuapp.com ';
+	: 'https://educaflix-app.herokuapp.com';
 
 export default {
 	URL_BACKEND,
