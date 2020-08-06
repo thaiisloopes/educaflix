9baa90f598949c636ef666da6d09ada2dfba1351
Thais <tlnunes@thoughtworks.com>
2020-08-05 21:34:41 -0300

    starts video register form and redirects to home after registering

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,102 +1,102 @@
-import React, { useState, useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
-import { Link } from 'react-router-dom';
 import FormField from '../../../components/FormField';
-import Button from '../../../components/Button'
-import useForm from '../../../hooks/useForm'
+import Button from '../../../components/Button';
+import useForm from '../../../hooks/useForm';
 
 function CadastroCategoria() {
-	const valoresIniciais = {
-		nome: '',
-		descricao: '',
-		cor: ''
-	}
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
 
-	const { valores, funcaoHandler, clearForm } = useForm(valoresIniciais)
+  const { valores, funcaoHandler, clearForm } = useForm(valoresIniciais);
 
-	const [categorias, setCategorias] = useState([]);
+  const [categorias, setCategorias] = useState([]);
 
-	useEffect(() => {
-		const URL = window.location.hostname.includes('localhost') 
-		? 'http://localhost:8080/categorias'
-		: 'https://educaflix-app.herokuapp.com/categorias';
+  useEffect(() => {
+    const URL = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://educaflix-app.herokuapp.com/categorias';
 
-		fetch(URL)
-			.then(async (respostaDoServidor) => {
-				const resposta = await respostaDoServidor.json();
-				setCategorias([
-					...resposta,
-				]);
-			});
-	}, []);
+    fetch(URL)
+      .then(async (respostaDoServidor) => {
+        const resposta = await respostaDoServidor.json();
+        setCategorias([
+          ...resposta,
+        ]);
+      });
+  }, []);
 
-	return (
-		<PageDefault>
-			<h1>Cadastro de Categoria: {valores.titulo} </h1>
+  return (
+    <PageDefault>
+      <h1>
+        Cadastro de Categoria:
+        {valores.titulo}
+      </h1>
 
-			<form onSubmit={function hadleSubmit(infosDoEvento) {
-				infosDoEvento.preventDefault();
-				setCategorias([
-					...categorias,
-					valores
-				]);
+      <form onSubmit={function hadleSubmit(infosDoEvento) {
+			  infosDoEvento.preventDefault();
+			  setCategorias([
+			    ...categorias,
+			    valores,
+			  ]);
 
-				clearForm(valoresIniciais)
-			}}>
-				
-				<FormField
-					label="Nome da Categoria"
-					type="text"
-					name="titulo"
-					value={valores.titulo}
-					onChange={funcaoHandler}
-				/>
+			  clearForm(valoresIniciais);
+      }}
+      >
 
-				<FormField
-					label="Descrição"
-					type="textarea"
-					name="descricao"
-					value={valores.descricao}
-					onChange={funcaoHandler}
-				/>
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="titulo"
+          value={valores.titulo}
+          onChange={funcaoHandler}
+        />
 
-				<FormField 
-					label="Cor"
-					type="color"
-					name="cor"
-					value={valores.cor}
-					onChange={funcaoHandler}
-				/>
-				
-				<Button>
-					Cadastrar
-				</Button>	
-			</form>
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="descricao"
+          value={valores.descricao}
+          onChange={funcaoHandler}
+        />
 
-			{categorias.lenght === 0 && (
-				<div>
-					{/* Cargando ... */ }
-					Loading...
-				</div>
-			)}
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={valores.cor}
+          onChange={funcaoHandler}
+        />
 
-			<ul>
-				{categorias.map((categoria) => {
-					return (
-						<li key={`${categoria.titulo}`}>
-							{categoria.titulo}
-						</li>
-					)
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
 
-				})}
+      {categorias.lenght === 0 && (
+      <div>
+        {/* Cargando ... */ }
+        Loading...
+      </div>
+      )}
 
-			</ul>
+      <ul>
+        {categorias.map((categoria) => (
+          <li key={`${categoria.titulo}`}>
+            {categoria.titulo}
+          </li>
+        ))}
+      </ul>
 
-			<Link to="/">
-				Ir para home
-			</Link>
-		</PageDefault>
-	)
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  );
 }
 
 export default CadastroCategoria;
--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,18 +1,43 @@
 import React from 'react';
+import {Link, useHistory} from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
-import { Link } from 'react-router-dom';
-
+import FormField from '../../../components/FormField';
+import useForm from '../../../hooks/useForm';
+import Button from '../../../components/Button';
 
 function CadastroVideo() {
-	return (
-		<PageDefault>
-			<h1>Cadastro de Video</h1>
+  const history = useHistory();
+  const { funcaoHandler, valores } = useForm({
 
-			<Link to="/cadastro/categoria">
-				Cadastrar Categoria
-			</Link>
-		</PageDefault>
-	)
+  });
+
+  return (
+    <PageDefault>
+      <h1>Cadastro de Video</h1>
+
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        alert('Vídeo cadastrado com sucesso!!');
+        history.push('/');
+      }}
+      >
+        <FormField
+          label="Título do Vídeo"
+          name="titulo"
+          value={valores.titulo}
+          onChange={funcaoHandler}
+        />
+
+        <Button type="submit">
+          Cadastrar
+        </Button>
+      </form>
+
+      <Link to="/cadastro/categoria">
+        Cadastrar Categoria
+      </Link>
+    </PageDefault>
+  );
 }
 
 export default CadastroVideo;
