a6835ab0e484350925aaa0929f5cd3ba219b98fc
Thais <tlnunes@thoughtworks.com>
2020-08-05 21:12:48 -0300

    fix home

--- src/pages/Home/App.js
+++ src/pages/Home/App.js
@@ -1,63 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import Menu from '../../components/Menu';
+import React, {useEffect, useState} from 'react';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-import PageDefault from '../../components/PageDefault'
-import Footer from '../../components/Footer';
-import dadosIniciais from '../../data/dados_iniciais.json';
-import categoriasRepository from '../../repositories/categorias'
+import PageDefault from '../../components/PageDefault';
+import categoriasRepository from '../../repositories/categorias';
 
 function Home() {
-    const [dadosIniciais, setDadosIniciais] = useState([]);
+  const [dadosIniciais, setDadosIniciais] = useState([]);
 
-    useEffect(() => {
-        categoriasRepository.getAllWithVideos()
-            .then((categoriasComVideos) => {
-                setDadosIniciais(categoriasComVideos);
-            })
-            .catch((err) => {
-                console.log(err.message);
-            });
-    }, []);
+  useEffect(() => {
+    categoriasRepository.getAllWithVideos()
+      .then((categoriasComVideos) => {
+        setDadosIniciais(categoriasComVideos);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
 
   return (
-    <PageDefault>
-        {dadosIniciais.length === 0 && (
-            <div>
-                Loading...
+    <PageDefault paddingAll={0}>
+      {dadosIniciais.length === 0 && (
+        <div>
+          Loading...
+        </div>
+      )}
+
+      {dadosIniciais.map((categoria, indice) => {
+        if (indice === 0) {
+          return (
+            <div key={categoria.id}>
+              <BannerMain
+                videoTitle={dadosIniciais[0].videos[0].titulo}
+                url={dadosIniciais[0].videos[0].url}
+                videoDescription="O que é Front-end? Trabalhando na área"
+              />
+
+              <Carousel
+                ignoreFirstVideo
+                category={dadosIniciais[0]}
+              />
             </div>
-        )}
+          );
+        }
 
-        {dadosIniciais.length >= 1 && (
-            <>
-            	<BannerMain
-            		videoTitle={dadosIniciais[0].videos[0].titulo}
-            		url={dadosIniciais[0].videos[0].url}
-            		videoDescription={ "O que é Front-end? Trabalhando na área"}
-            	/>
-
-            	<Carousel
-            		ignoreFirstVideo
-            		category={dadosIniciais[0]}
-            	/>
-            </>
-        )};
-
-    	{/* <Carousel
-    		category={dadosIniciais.categorias[1]}
-    	/>
-
-    	<Carousel
-    		category={dadosIniciais.categorias[2]}
-    	/>
-
-        <Carousel
-            category={dadosIniciais.categorias[3]}
-        />
-
-        <Carousel
-            category={dadosIniciais.categorias[4]}
-        /> */} 
+        return (
+          <Carousel
+            key={categoria.id}
+            category={categoria}
+          />
+        );
+      })}
+      ;
 
     </PageDefault>
   );
